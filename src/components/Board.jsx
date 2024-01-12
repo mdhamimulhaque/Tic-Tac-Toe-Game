c3617f008a7cfd0e92870c5873f0ad2df28779c7
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-01-12 15:18:13 +0600

    square value handle and changed by click every square [done]

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,59 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squareValue, setSquareValue] = useState([Array(9).fill(null)]);
+
+  const handleSquareClick = (i) => {
+    const newSquare = squareValue.slice();
+    newSquare[i] = "X";
+    setSquareValue(newSquare);
+  };
+
   return (
-    <section className="col-span-8 min-h-screen flex justify-center items-center">
-      <div>
-        <Square />
-        <Square />
-        <Square />
+    <section className="col-span-8 min-h-screen flex flex-col justify-center items-center">
+      <div className="flex">
+        <Square
+          value={squareValue[0]}
+          handleSquareClick={() => handleSquareClick(0)}
+        />
+        <Square
+          value={squareValue[1]}
+          handleSquareClick={() => handleSquareClick(1)}
+        />
+        <Square
+          value={squareValue[2]}
+          handleSquareClick={() => handleSquareClick(2)}
+        />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square
+          value={squareValue[3]}
+          handleSquareClick={() => handleSquareClick(3)}
+        />
+        <Square
+          value={squareValue[4]}
+          handleSquareClick={() => handleSquareClick(4)}
+        />
+        <Square
+          value={squareValue[5]}
+          handleSquareClick={() => handleSquareClick(5)}
+        />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square
+          value={squareValue[6]}
+          handleSquareClick={() => handleSquareClick(6)}
+        />
+        <Square
+          value={squareValue[7]}
+          handleSquareClick={() => handleSquareClick(7)}
+        />
+        <Square
+          value={squareValue[8]}
+          handleSquareClick={() => handleSquareClick(8)}
+        />
       </div>
     </section>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,13 @@
-const Square = () => {
+/* eslint-disable react/prop-types */
+
+const Square = ({ value, handleSquareClick }) => {
   return (
-    <section className="col-span-4 h-28 w-28 border-2 border-gray-400 rounded-sm text-3xl flex justify-center items-center">
-      1
-    </section>
+    <button
+      className="col-span-4 h-28 w-28 border-2 border-gray-400 rounded-sm text-3xl flex justify-center items-center"
+      onClick={handleSquareClick}
+    >
+      {value}
+    </button>
   );
 };
 
